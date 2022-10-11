43e086e1a6ed3864a0d9832bef7bda24551bc3b7
ZarNizza <gormaster@mail.ru>
2022-10-12 00:09:13 +0400

    List of Persons (1.py)

---
+++ 1.py
@@ -0,0 +1,20 @@
+# create List of Person instances
+
+class Person:
+  def __init__(self, name,nick):
+    self.name = name
+    self.nick = nick
+
+data = [
+  {'name':'Ivan1','nick':'Ivanych1'},
+  {'name':'Ivan2','nick':'Ivanych2'},
+  {'name':'Ivan3','nick':'Ivanych3'}]
+list_of_Persons = []
+
+# main
+for i in data:
+  list_of_Persons.append(Person(**i))
+
+# print result
+for pers in list_of_Persons:
+  print(pers.name, pers.nick)
