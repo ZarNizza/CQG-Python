585681b2c7b9deb8ad3fdc306f462791b40f7741
ZarNizza <gormaster@mail.ru>
2022-10-17 12:16:13 +0400

    archive version

---
+++ 1_old.py
@@ -0,0 +1,38 @@
+#! python3
+
+# as I recall, in original example the final ":" after class name was absent ))
+
+# create List of Person instances
+
+def main():
+    class Person:
+        def __init__(self, name, nick):
+            self.name = name
+            self.nick = nick
+
+    data = [
+        {'name': 'Ivan1', 'nick': 'Ivanych1'},
+        {'name': 'Ivan2', 'nick': 'Ivanych2'},
+        {'name': 'Ivan3', 'nick': 'Ivanych3'}]
+    list_of_Persons = []
+
+
+    # previous version
+    #for i in data:
+    #    list_of_Persons.append(Person(**i))
+
+    # tadaaaam!..
+    #def p(i):
+    #    return Person(**i)
+    #list_of_Persons = list(map(p,data))
+
+    # tadabadaaam!..
+    list_of_Persons = list(map(lambda i: Person(**i), data))
+
+
+    # print result
+    for pers in list_of_Persons:
+        print(pers.name, pers.nick)
+
+
+main()
